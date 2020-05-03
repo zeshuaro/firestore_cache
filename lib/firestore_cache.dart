9bc13dc329712bd0c2ca4e813d51a3bf11d50d41
zeshuaro <joshuaystang@gmail.com>
2020-05-03 15:30:26 +1000

    Add to string method to exceptions (#6)

--- lib/firestore_cache.dart
+++ lib/firestore_cache.dart
@@ -81,20 +81,28 @@ class FirestoreCache {
 }
 
 class CacheDocDoesNotExist implements Exception {
-  final String cause = '''Your cache document does not exist on Firestore, 
+  final String message = '''Your cache document does not exist on Firestore, 
             which means you will always be fetching your documents from the server. 
             Create your cache document on Firestore first 
             with your specified field name of a timestamp.''';
 
   CacheDocDoesNotExist();
+
+  String toString() {
+    return message;
+  }
 }
 
 class CacheDocFieldDoesNotExist implements Exception {
-  final String cause =
+  final String message =
       '''Your cache document does not contain your specified field, 
             which means you will always be fetching your documents from the server. 
             Create your specified filed name with a timestamp 
             in your cache document on Firestore first.''';
 
   CacheDocFieldDoesNotExist();
+
+  String toString() {
+    return message;
+  }
 }
