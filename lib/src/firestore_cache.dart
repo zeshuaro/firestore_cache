948e64c3d8452e8955f5ddb2935218febec25003
Joshua <joshuaystang@gmail.com>
2021-02-02 22:35:11 +1100

    Update dependency versions

--- lib/src/firestore_cache.dart
+++ lib/src/firestore_cache.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'exceptions.dart';
